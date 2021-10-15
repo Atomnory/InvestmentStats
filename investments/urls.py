17bcef7e45541f1486ba2ec4455375ac2d9471a6
Atomnory <easports2011@live.ru>
2021-10-15 20:49:44 +0300

    Add matplotlib, add creating test pie graph

--- investments/urls.py
+++ investments/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index_page, name='index')
+    path('', views.index_page, name='index'),
+    path('load_graph', views.load_graph, name='load_graph')
 ]
--- investments/views.py
+++ investments/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from .models import Portfolio
+import matplotlib.pyplot as plt
 
 
 def index_page(request):
     portfolio_list = Portfolio.objects.all()
 
     index_page_data = {
-        'portfolio': portfolio_list
+        'portfolio': portfolio_list,
+        'is_graph_exist': False
     }
 
     return render(request, 'investments/index.html', index_page_data)
+
+
+def load_graph(request):
+    get_graph()
+    return index_page(request)
+
+
+def get_graph():
+    portfolio_list = Portfolio.objects.all()
+    data = [x.price * x.quantity for x in portfolio_list]
+    labels = [x.ticker for x in portfolio_list]
+
+    plt.pie(data, labels=labels, autopct='%1.1f%%')
+    # fig, ax = plt.subplots()
+    # ax.pie()
+    plt.savefig('portfolio_pie.png')
+    # plt.show()
+
