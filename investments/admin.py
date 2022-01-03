c937524b02edbb095d0d72d08868c549605d9e1f
Atomnory <easports2011@live.ru>
2022-01-03 20:48:45 +0300

    Update admin panel, add stock_stop_list.txt to git

--- investments/admin.py
+++ investments/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 from .models import Security, ExchangeRate, Portfolio, PortfolioItem
 
 
-admin.site.register(Security)
-admin.site.register(ExchangeRate)
-admin.site.register(Portfolio)
+class SecurityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ticker', 'not_found_on_market')
+
+    list_filter = ['not_found_on_market']
+
+
+class ExchangeRateAdmin(admin.ModelAdmin):
+    list_display = ('last_updated', 'eur_rate', 'rub_rate')
+
+
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('investor', 'name')
+
+
+admin.site.register(Security, SecurityAdmin)
+admin.site.register(ExchangeRate, ExchangeRateAdmin)
+admin.site.register(Portfolio, PortfolioAdmin)
 admin.site.register(PortfolioItem)
